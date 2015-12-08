d63a60161325a9c097c713ac74f5d6ea119b8470
Petri Matilainen <pa.matilainen@gmail.com>
2015-12-08 12:50:09 +0200

    Cleaning up

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express  = require('express');
 
 var app = express();
-
 require('./routes')(app);
-
 app.use(express.static(__dirname + '/public'));
-
 app.listen(process.env.PORT || 8080);
-console.log('Listening port 8080');
 
 exports = module.exports = app;
